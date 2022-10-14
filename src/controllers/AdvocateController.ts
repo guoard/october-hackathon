578a3c913d5ac0d8d893c1fc0edbd9956a03304b
Ali Afsharzadeh <afsharzadeh1998@gmail.com>
2022-10-14 10:26:38 +0330

    chore: Complete advocte controller

--- src/controllers/AdvocateController.ts
+++ src/controllers/AdvocateController.ts
@@ -19,7 +19,7 @@ export const getAdvocate = async (req: Request, h: ResponseToolkit) => {
 };
 
 export const listAdvocates = async (req: Request, h: ResponseToolkit) => {
-  const advocates = await Advocate.find({}).sort({ createdAt: -1 });
+  const advocates = await Advocate.find().sort({ createdAt: -1 });
 
   return h.response(advocates);
 };
@@ -41,7 +41,7 @@ export const createAdvocate = async (req: Request, h: ResponseToolkit) => {
     if (company) {
       thisCompany = await Company.findById(company);
       if (!thisCompany) {
-        return false;
+        throw Boom.badRequest("Company does not exists");
       }
     }
 
@@ -67,16 +67,11 @@ export const createAdvocate = async (req: Request, h: ResponseToolkit) => {
 
     if (thisCompany) {
       thisCompany.advocates.push(advocate[0]._id);
+      await thisCompany.save({ session });
     }
-
-    await thisCompany.save({ session });
 
     return advocate[0];
   });
-
-  if (!createdAdvocate) {
-    return Boom.notFound("Company does not exists");
-  }
 
   const populatedAdvocate = await createdAdvocate.populate({
     path: "company",
@@ -86,8 +81,6 @@ export const createAdvocate = async (req: Request, h: ResponseToolkit) => {
   return h.response(populatedAdvocate).code(201);
 };
 
-//TODO: check populate updated advocate
-//TODO: use transaction to update advocate
 export const updateAdvocate = async (req: Request, h: ResponseToolkit) => {
   const { id } = req.params;
   const {
@@ -105,37 +98,63 @@ export const updateAdvocate = async (req: Request, h: ResponseToolkit) => {
     return Boom.notFound("Advocate does not exists");
   }
 
-  if (company) {
-    const thisCompany = await Company.findById(company);
-    if (!thisCompany) {
-      return Boom.notFound("Company does not exists");
+  const updatedAdvocate = await runInTransaction(async (session) => {
+    let thisCompany: any;
+
+    if (company) {
+      thisCompany = await Company.findById(company);
+      if (!thisCompany) {
+        throw Boom.badRequest("Company does not exists");
+      }
     }
-  }
 
-  //TODO: Check if it is same picture
-  advocate.deleteProfilePic();
+    //TODO: Check if it is same picture
+    advocate.deleteProfilePic();
 
-  let fileName: string | null = null;
-  if (profile_pic) {
-    fileName = await savePhoto(profile_pic);
-  }
+    let fileName: string | null = null;
+    if (profile_pic) {
+      fileName = await savePhoto(profile_pic);
+    }
 
-  advocate.name = name;
-  advocate.profile_pic = fileName;
-  advocate.short_bio = short_bio;
-  advocate.long_bio = long_bio;
-  advocate.advocate_years_exp = advocate_years_exp;
-  advocate.company = company;
-  advocate.links = links;
+    //TODO: do not delete if is same with next company
+    //Remove this advocate from privious company
+    if (advocate.company) {
+      const previousComapny = await Company.findById(advocate.company);
+      previousComapny!.advocates = previousComapny!.advocates.filter(
+        (element: any) => {
+          element._id !== advocate._id;
+        }
+      );
+      await previousComapny!.save({ session });
+    }
 
-  const populatedAdvocate = await advocate.populate({
+    //Add this advocage to new company
+    if (thisCompany) {
+      thisCompany.advocates.push(advocate._id);
+      await thisCompany.save({ session });
+    }
+
+    advocate.name = name;
+    advocate.profile_pic = fileName;
+    advocate.short_bio = short_bio;
+    advocate.long_bio = long_bio;
+    advocate.advocate_years_exp = advocate_years_exp;
+    advocate.company = company;
+    advocate.links = links;
+
+    await advocate.save({ session });
+
+    return advocate;
+  });
+
+  const populatedAdvocate = await updatedAdvocate.populate({
     path: "company",
-    select: "id name logo",
+    select: "id name logo -advocates",
   });
 
   return h.response(populatedAdvocate);
 };
-//TODO: remove advocate from associated company
+
 export const deleteAdvocate = async (req: Request, h: ResponseToolkit) => {
   const { id } = req.params;
 
@@ -144,8 +163,19 @@ export const deleteAdvocate = async (req: Request, h: ResponseToolkit) => {
     return Boom.notFound("Advocate does not exists");
   }
 
-  advocate.deleteProfilePic();
-  await advocate.delete();
+  runInTransaction(async (session) => {
+    if (advocate.company) {
+      const comapny = await Company.findById(advocate.company);
+      comapny!.advocates = comapny!.advocates.filter((element: any) => {
+        element._id !== advocate._id;
+      });
+      await comapny!.save({ session });
+    }
+
+    advocate.deleteProfilePic();
+
+    await advocate.delete({ session });
+  });
 
   return h.response().code(204);
 };
